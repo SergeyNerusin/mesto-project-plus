afa40b4071151ad22e77f304e02981fbb3124456
Nerusin Sergey <s.nerusin@yandex.ru>
2023-04-29 15:29:39 +0300

    conntecting mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+
+const server = '127.0.0.1:27017'; // REPLACE WITH YOUR OWN SERVER
+const database = 'mestodb'; // REPLACE WITH YOUR OWN DB NAME
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb://${server}/${database}`);
+    console.log('MongoDB connected!!');
+
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log('Failed to connect to MongoDB', err);
+  }
+};
+
+connectDB();
