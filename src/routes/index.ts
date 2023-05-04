25b02cbf69295b267d74dcbf0ca2177c0d3aea51
Nerusin Sergey <s.nerusin@yandex.ru>
2023-05-04 10:06:19 +0300

    modified: routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import userRouter from './user-routers';
+import cardRouter from './cards-routers';
 
 const router = Router();
-
+router.use('/users', userRouter);
+router.use('/cards', cardRouter);
 export default router;
