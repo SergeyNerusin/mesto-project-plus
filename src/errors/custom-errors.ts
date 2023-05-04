92fbbc175eb4c1bd3e37dda32ddc14c9042ac54b
Nerusin Sergey <s.nerusin@yandex.ru>
2023-05-04 19:05:46 +0300

    add: errors/custom-errors.ts

---
+++ src/errors/custom-errors.ts
@@ -0,0 +1,23 @@
+class AppError extends Error {
+  status: number;
+
+  constructor(code: number, message: string) {
+    super();
+    this.status = code;
+    this.message = message;
+  }
+
+  static badRequest(message: string) {
+    return new AppError(400, message);
+  }
+
+  static notFound(message: string) {
+    return new AppError(404, message);
+  }
+
+  static serverError(message: string) {
+    return new AppError(500, message);
+  }
+}
+
+export default AppError;
