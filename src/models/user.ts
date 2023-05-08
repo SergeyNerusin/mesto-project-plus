deaa3fe2555e96ace3816b21fc9bc4417ff2abb5
Nerusin Sergey <s.nerusin@yandex.ru>
2023-05-08 03:37:51 +0300

    user shema: added email, password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,26 +1,44 @@
 import { model, Schema, Document } from 'mongoose';
+import validator from 'validator';
 import regUrl from '../utils/constant';
 
 export interface IUser extends Document {
+  email: string;
+  password: string;
   name: string;
   about: string;
   avatar: string;
 }
 
 const userSchema = new Schema<IUser>({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'e-mail is required'],
+    validate: {
+      validator: (v: string) => validator.isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'password is required'],
+    select: false,
+    minlength: 8,
+  },
   name: {
     type: String,
-    required: true,
     trim: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив-Кусто',
   },
   about: {
     type: String,
-    required: true,
     trim: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
@@ -29,6 +47,8 @@ const userSchema = new Schema<IUser>({
       validator: (v: string) => regUrl.test(v),
       message: 'Incorrect avatar link',
     },
+    default:
+      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
