c971176701bce8cbf0dc1b68ddbc84bafa4ded16
Nerusin Sergey <s.nerusin@yandex.ru>
2023-05-16 18:54:34 +0300

    add: /validation/card-validation.ts & modified: routes/cards-routers.ts

---
+++ src/validation/card-validation.ts
@@ -0,0 +1,15 @@
+import { Joi, celebrate } from 'celebrate';
+import { regExpUrl, regExpUrlPicture } from '../utils/constant';
+
+export const cardDataValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().pattern(regExpUrl).pattern(regExpUrlPicture).required(),
+  }),
+});
+
+export const idCardValidation = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24).required(),
+  }),
+});
